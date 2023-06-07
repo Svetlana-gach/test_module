1f1772db90854d05872b0304a8b9695844f880a6
Svetlana-gach <77901136+Svetlana-gach@users.noreply.github.com>
2023-06-07 12:56:53 +0300

    Create .description.php

---
+++ local/modules/testing.dispatchers/install/components/testing/dispatchers.list/.description.php
@@ -0,0 +1,13 @@
+<?php
+if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
+
+use Bitrix\Main\Localization\Loc;
+
+$arComponentDescription = [
+    "NAME" => GetMessage("testing.dispatchers_TITLE"),
+    "DESCRIPTION" => GetMessage("testing.dispatchers_DESCR"),
+    "PATH" => [
+        "ID" => "Список Диспетчеров",
+    ],
+];
+?>
